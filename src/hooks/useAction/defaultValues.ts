ffed1548b685e85d4898ef3c1cdd50e868497f20
odutradev <joao.vitornl@gmail.com>
2026-05-29 15:14:18 -0300

    refactor: Cleaning

--- src/hooks/useAction/defaultValues.ts
+++
@@ -1,7 +0,0 @@
-import type { ToastMessages } from "./types";
-
-export const defaultToastMessages: ToastMessages = {
-    success: "Request successful",
-    pending: "Initializing request",
-    error: "Error in request"
-};
